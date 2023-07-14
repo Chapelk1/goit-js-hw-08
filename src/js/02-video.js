07bc379f393156921fd93fab624c78b0d6136965
Chapelk1 <klepach154789@gmail.com>
2023-07-15 01:03:21 +0300

    add all

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const VIDEO_TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function timeUpdate(data) {
+  localStorage.setItem(VIDEO_TIME_KEY, JSON.stringify(data.seconds));
+  console.log(localStorage);
+}
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem(VIDEO_TIME_KEY))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+const FORM_DATA_KEY = 'feedback-form-state';
+const formFeedbackData = {
+  ...JSON.parse(localStorage.getItem(FORM_DATA_KEY)),
+};
+populateTextInput();
 
+refs.form.addEventListener('input', throttle(onChangeInValue, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onChangeInValue(e) {
+  const inputName = e.target.name;
+  const inputValue = e.target.value;
+  formFeedbackData[inputName] = inputValue;
+  localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formFeedbackData));
+}
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    const {
+      elements: { email, message },
+    } = e.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert('Усі поля повинні бути заповнені!');
+  }
+  console.log(JSON.parse(localStorage.getItem(FORM_DATA_KEY)));
+  e.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA_KEY);
+  Object.keys(formFeedbackData).forEach(key => delete formFeedbackData[key]);
+}
+
+function populateTextInput() {
+  const formData = JSON.parse(localStorage.getItem(FORM_DATA_KEY));
+  if (formData) {
+      for (let key in formData) {
+        refs.form[key].value = formData[key]
+      }
+    return;
+  }
+  localStorage.setItem(FORM_DATA_KEY, JSON.stringify(formFeedbackData));
+}
